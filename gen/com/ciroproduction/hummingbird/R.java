d88165be9e67aa41f34e78cf5d95d40018db3eea
ericcj24 <dannycj11@gmail.com>
2014-05-21 14:25:10 -0700

    conversation entry

--- gen/com/ciroproduction/hummingbird/R.java
+++ gen/com/ciroproduction/hummingbird/R.java
@@ -150,7 +150,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int indicator=0x7f020001;
-        public static final int shadow=0x7f020002;
+        public static final int new_indicator=0x7f020002;
+        public static final int shadow=0x7f020003;
     }
     public static final class id {
         public static final int content_frame=0x7f040006;
@@ -160,11 +161,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int menu_frame=0x7f040007;
         public static final int none=0x7f040004;
         public static final int right=0x7f040001;
-        public static final int row_contact_title=0x7f04000a;
+        public static final int row_contact_title=0x7f040009;
+        public static final int row_conversation=0x7f04000a;
+        public static final int row_conversation_frame=0x7f04000c;
+        public static final int row_delete_ic=0x7f04000d;
+        public static final int row_delete_text=0x7f04000e;
         public static final int row_icon=0x7f040008;
-        public static final int row_title=0x7f040009;
+        public static final int row_title=0x7f04000b;
         public static final int selected_view=0x7f040005;
-        public static final int slidingmenumain=0x7f04000b;
+        public static final int slidingconversationlistlayout=0x7f04000f;
+        public static final int slidingmenumain=0x7f040010;
     }
     public static final class integer {
         public static final int num_cols=0x7f080000;
@@ -172,11 +178,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class layout {
         public static final int content_frame=0x7f030000;
         public static final int list=0x7f030001;
-        public static final int menu=0x7f030002;
-        public static final int menu_frame=0x7f030003;
-        public static final int row=0x7f030004;
-        public static final int row_contact=0x7f030005;
-        public static final int slidingmenumain=0x7f030006;
+        public static final int menu_frame=0x7f030002;
+        public static final int row_contact=0x7f030003;
+        public static final int row_conversation=0x7f030004;
+        public static final int row_conversation_frame=0x7f030005;
+        public static final int row_slide=0x7f030006;
+        public static final int slidingmenumain=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
--- src/com/ciroproduction/hummingbird/fragments/ConversationListFragment.java
+++ src/com/ciroproduction/hummingbird/fragments/ConversationListFragment.java
@@ -1,17 +1,21 @@
 package com.ciroproduction.hummingbird.fragments;
 
+import com.ciroproduction.hummingbird.MainActivity;
 import com.ciroproduction.hummingbird.R;
 import com.ciroproduction.hummingbird.R.id;
 import com.ciroproduction.hummingbird.R.layout;
+import com.ciroproduction.hummingbird.fragments.ContactMenuFragment.ContactAdapter;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ConversationListFragment extends ListFragment {
@@ -22,13 +26,56 @@ public class ConversationListFragment extends ListFragment {
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		
+		String[] colors = getResources().getStringArray(R.array.color_names);
 		SampleAdapter adapter = new SampleAdapter(getActivity());
-		for (int i = 0; i < 20; i++) {
-			adapter.add(new SampleItem("Contact Name", android.R.drawable.sym_def_app_icon));
+		for (int i = 0; i < colors.length; i++) {
+			adapter.add(new SampleItem("Contact Name "+colors[i], android.R.drawable.sym_def_app_icon));
 		}
 		setListAdapter(adapter);
 	}
 
+	/** --------------------------------------*/
+	public void onListItemClick(ListView lv, View v, int position, long id) {
+		Fragment newContent = null;
+		switch (position) {
+		case 0:
+			newContent = new ConversationFragment(R.color.red);
+			break;
+		case 1:
+			newContent = new ConversationFragment(R.color.green);
+			break;
+		case 2:
+			newContent = new ConversationFragment(R.color.blue);
+			break;
+		case 3:
+			newContent = new ConversationFragment(android.R.color.white);
+			break;
+		case 4:
+			newContent = new ConversationFragment(android.R.color.black);
+			break;
+		}
+		if (newContent != null)
+			switchFragment(newContent);
+	}
+
+	// the meat of switching the above fragment
+	private void switchFragment(Fragment fragment) {
+		if (getActivity() == null)
+			return;
+		
+		if (getActivity() instanceof MainActivity) {
+			MainActivity fca = (MainActivity) getActivity();
+			fca.switchContent(fragment);
+		} /**else if (getActivity() instanceof ResponsiveUIActivity) {
+			ResponsiveUIActivity ra = (ResponsiveUIActivity) getActivity();
+			ra.switchContent(fragment);
+		}*/
+	}
+	/** --------------------------------------*/
+	
+	
+	
 	private class SampleItem {
 		public String tag;
 		public int iconRes;
@@ -46,7 +93,7 @@ public class ConversationListFragment extends ListFragment {
 
 		public View getView(int position, View convertView, ViewGroup parent) {
 			if (convertView == null) {
-				convertView = LayoutInflater.from(getContext()).inflate(R.layout.row, null);
+				convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_conversation, null);
 			}
 			ImageView icon = (ImageView) convertView.findViewById(R.id.row_icon);
 			icon.setImageResource(getItem(position).iconRes);
